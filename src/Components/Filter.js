f86f59428a62046248de2b8bb5cfc992aa54d0c7
Charlie Wray <charlesmwray@gmail.com>
2017-02-20 18:18:44 -0800

    Restyles filter bar for mobile.

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -11,26 +11,23 @@ const Filter = (props) => {
     }
     return (
         <div className="filter-container">
-            <div className="filter-item search form-group">
-                <h4>Search</h4>
+            <div className="filter-section">
                 <form onSubmit={ (e) => { search(e) } }>
                     <input className="form-control search" type="text" id="search" />
-                    <button className="btn btn-default first" type="submit">Search</button>
-                    <button className="btn btn-danger" type="reset" onClick={ props.resetSearch }>Reset</button>
+                    <button className="btn btn-primary first" type="submit">Search</button>
+                    <button className="btn btn-default" type="reset" onClick={ props.resetSearch }>Reset</button>
                 </form>
             </div>
-            <div className="filter-item form-group">
-                <h4>Filter</h4>
-                <select className="form-control" onChange={ (e) => { filter(e) } } defaultValue="select">
-                    <option disabled value="select">-- Select</option>
+            <div className="filter-section">
+                <label>Filter</label>
+                <select className="form-control filter" onChange={ (e) => { filter(e) } }>
                     <option value="None">None</option>
                     <option value="Business Trip">Business Trip</option>
                     <option value="Vacation">Vacation</option>
                 </select>
             </div>
-            <div className="filter-item">
-                <h4>Create</h4>
-                <button className="btn btn-default" onClick={ props.createNewTrip }>New Trip</button>
+            <div className="filter-section pull-right">
+                <button className="btn btn-default" onClick={ props.createNewTrip }>Create New Trip</button>
             </div>
         </div>
     );
